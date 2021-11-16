690ada879051a9ab14f1fbadf47c1a5730014384
Magnus Fulton (@YakimaProgrammer) <magnus@magnusfulton.com>
2021-11-15 20:25:56 -0800

    Added a 404 page

--- src/App/index.js
+++ src/App/index.js
@@ -9,7 +9,7 @@ import {
 import style from "./index.module.css";
 import crazed from "../CultureCollage/resources/crazed.jpg";
 
-function Projects() {
+function ProjectList() {
   return (
     <div>
       <h1 className={style.title}>Photography 101</h1>
@@ -25,13 +25,20 @@ function Projects() {
   )
 }
 
+function Error404() {
+  return (
+    <h1 className={style.title}>Error 404: Not found</h1>
+  )
+}
+
 export function App() {
   return (
     <div className={style.overrideLinkStyle}>
       <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/culture-collage" element={<CultureCollage />} />
-          <Route path="" element={<Projects />} />
+          <Route path="" element={<ProjectList />} />
+          <Route path="*" element={<Error404 />} />
         </Routes>
       </BrowserRouter>
     </div>
